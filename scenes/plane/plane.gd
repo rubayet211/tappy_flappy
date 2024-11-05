562125c1f87962ff1987a87d7ce23a5bf23bcda4
Rhyme Rubayet <rubayet211@gmail.com>
2024-11-05 20:30:35 +0600

    Added flight when pressed spacebar, added floor and ceiling. When floor is touched, game stops

--- scenes/plane/plane.gd
+++ scenes/plane/plane.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
 const GRAVITY: float = 600.0
+const POWER: float = -600.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,4 +17,17 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	velocity.y += GRAVITY * delta
+	fly()
 	move_and_slide()
+	
+	if is_on_floor() ==  true:
+		die()
+
+func fly() -> void:
+	if Input.is_action_just_pressed("fly") == true:
+		velocity.y = POWER
+
+
+func die() -> void:
+	animated_sprite_2d.stop()
+	set_physics_process(false)
